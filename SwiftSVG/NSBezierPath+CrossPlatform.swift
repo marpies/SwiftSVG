faa70909250131a193507d5b64942adad61ad1f8
Marcel Piešťanský <marcel@piestansky.cz>
2024-03-20 12:26:23 +0100

    Fixed build on macOS 14

--- SwiftSVG/NSBezierPath+CrossPlatform.swift
+++ SwiftSVG/NSBezierPath+CrossPlatform.swift
@@ -44,10 +44,14 @@ public extension NSBezierPath {
                     path.move(to: points[0])
                 case .lineTo:
                     path.addLine(to: points[0])
-                case .curveTo:
+                case .curveTo, .cubicCurveTo:
                     path.addCurve(to: points[2], control1: points[0], control2: points[1])
                 case .closePath:
                     path.closeSubpath()
+                case .quadraticCurveTo:
+                    path.addQuadCurve(to: points[1], control: points[0])
+                @unknown default:
+                    break
                 }
             }
             return path
